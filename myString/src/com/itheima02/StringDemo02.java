6367467f5a8e9afbf7d2c4bb4c089aa0c036d4dd
wantairanwtr <email>
2021-10-22 13:41:03 +0800

    myString

---
+++ myString/src/com/itheima02/StringDemo02.java
@@ -0,0 +1,18 @@
+package com.itheima02;
+
+public class StringDemo02 {
+    public static void main(String[] args){
+        char[] chs={'a','b','c'};
+        String s1=new String(chs);
+        String s2=new String(chs);
+        String s3="abc";
+        String s4="abc";
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+        System.out.println(s3==s4);
+        System.out.println("-----------");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+    }
+}
